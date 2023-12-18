aa8770506eea3a418b35cd6afc588bb6264b6300
Roberto Vargas Rojas <39863282+robertovargas97@users.noreply.github.com>
2023-12-17 22:26:34 -0600

    Custom Hooks

---
+++ react_course_fh/06-custom-hooks/todoReducer.ts
@@ -0,0 +1,34 @@
+import { Todo, TodoAction } from './Todo.types'
+
+export const todoReducer = (state: Todo[] = [], action: TodoAction) => {
+  const removeTodo = (elementId: number) => {
+    return state.filter((todo) => todo.id !== elementId)
+  }
+
+  const toggleDoneForTodo = (elementId: number) => {
+    return state.map((todo) => {
+      if (todo.id === elementId) {
+        todo = { ...todo, done: !todo.done }
+      }
+      return todo
+    })
+  }
+
+  switch (action.type) {
+    case 'Add':
+      state = [...state, action.payload]
+      break
+    case 'Delete':
+      state = removeTodo(action.payload.id)
+      break
+    case 'Toggle':
+      state = toggleDoneForTodo(action.payload.id)
+
+      break
+
+    default:
+      break
+  }
+
+  return state
+}
